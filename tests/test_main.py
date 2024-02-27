fcb23d9ed56ac71ff1be816cfd539b79e303b990
craig <73603201+craig-lilley@users.noreply.github.com>
2024-02-27 19:13:22 +0000

    Restructure completed and all tests are passing.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,15 @@
-
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from src.main import read_data
+# We will write our test cases in this file
+from backend.data_processing import read_data
 import pandas as pd
 
 #test read data function
 def test_read_data():
     # test reading a csv file
-    assert read_data('test_data/imdb_top_1000.csv').equals(pd.read_csv('test_data/imdb_top_1000.csv'))
+    assert read_data('data/imdb_top_1000.csv').equals(pd.read_csv('data/imdb_top_1000.csv'))
     # test reading an excel file
-    assert read_data('test_data/bfi-weekend-box-office-report-2024-02-16-18.xlsx').equals(pd.read_excel('test_data/bfi-weekend-box-office-report-2024-02-16-18.xlsx'))
+    assert read_data('data/bfi-weekend-box-office-report-2024-02-16-18.xlsx').equals(pd.read_excel('data/bfi-weekend-box-office-report-2024-02-16-18.xlsx'))
     # test reading a json file
-    assert read_data('test_data/News_Category_Dataset_v3.json').equals(pd.read_json('test_data/News_Category_Dataset_v3.json', lines=True))
+    assert read_data('data/News_Category_Dataset_v3.json').equals(pd.read_json('data/News_Category_Dataset_v3.json', lines=True))
     # test reading an invalid file type
     assert read_data('data.txt') == 'Invalid file type'
 
